beb9301c7a64fead2cdc531f434c54391f64abf5
Melomast <shermannut@gmail.com>
2024-03-19 14:36:23 +0600

    First Hello world Commit

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+/*
+func main() {
+	//Declaration
+	var name string
+
+	//Initiallization
+	name = "German"
+
+	//Declaration and Initiallization
+	name2 := "Mary"
+	var name3 string = "Ivan"
+
+	fmt.Println(name)
+	fmt.Println(name2)
+	fmt.Println(name3)
+}
+*/
+/*
+func main() {
+	var a bool = true
+	var b int = 5
+	var c float32 = 3.14
+	var d string = "German"
+
+	fmt.Println("Boolean:", a)
+	fmt.Println("Integer:", b)
+	fmt.Println("Float:", c)
+	fmt.Println("String:", d)
+}
+*/
+/*
+func main() {
+	const pi = 3.14 //Константа
+
+	fmt.Println(pi)
+}
+*/
+
+func main() {
+	// +
+	// -
+	// *
+	// /
+	// %
+
+	a := 10
+	b := 31
+
+	result := b % a
+	fmt.Println(result)
+}
